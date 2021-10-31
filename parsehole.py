da157d4e41e673968c00c46128d9df24becf542a
L3viathan <git@l3vi.de>
2021-10-31 21:53:43 +0100

    Add a whole bunch of comments to explain the code.
    I usually don't like comments (or consider them warning signs). Here,
    the entire project is a warning sign.

--- parsehole.py
+++ parsehole.py
@@ -5,13 +5,17 @@ import hashlib
 from io import BytesIO
 from collections import deque
 
+# All Token subclasses:
 TOKENS = []
+# All Tree subclasses:
 TREES = []
+# map from Token/Tree subclass name to the class itself:
 NAMES = {}
+# all precedence levels that are used:
 LEVELS = set()
 
 
-# bytecode instructions:
+# bytecode instructions (see the __build_class__ shenanigans below):
 BUILD_LIST = dis.opmap["BUILD_LIST"]
 STORE_NAME = dis.opmap["STORE_NAME"]
 LOAD_NAME = dis.opmap["LOAD_NAME"]
@@ -21,6 +25,7 @@ POP_TOP = dis.opmap["POP_TOP"]
 MAKE_FUNCTION = dis.opmap["MAKE_FUNCTION"]
 
 
+# tiny mixin so we don't have to implement this on Rule, TokenMeta and TreeMeta
 class Addable:
     def __add__(self, other):
         if not isinstance(other, Addable):
@@ -30,6 +35,7 @@ class Addable:
         return Rule([self, other])
 
 
+# this represents one rule of a tree, e.g. expr -> expr op expr
 class Rule(Addable):
     def __init__(self, parts):
         self.parts = parts
@@ -44,21 +50,24 @@ class Rule(Addable):
         yield from iter(self.parts)
 
 
+# Metaclass for Token; mostly needed for supporting __add__
 class TokenMeta(type, Addable):
     def __repr__(self):
-        return self.__name__  # TODO: getitem?
+        return self.__name__
 
     @property
     def level(self):
         return self.kwargs.get("level", 0)
 
 
+# a terminal node
 class Token(metaclass=TokenMeta):
     def __init__(self, string):
         self.value = string
 
     def __init_subclass__(cls, **kwargs):
         cls.kwargs = kwargs
+        # matching rule is in docstring:
         cls.regex = re.compile(cls.__doc__)
         TOKENS.append(cls)
         NAMES[cls.__name__] = cls
@@ -70,6 +79,9 @@ class Token(metaclass=TokenMeta):
         return self.value
 
 
+# to support "recursion" inside a class definition, we put a placeholder into
+# the class dict. This placeholder knows the name of the Tree and will
+# eventually be replaced by the real class.
 class Placeholder(Addable):
     def __init__(self, name):
         self.name = name
@@ -77,6 +89,8 @@ class Placeholder(Addable):
     def __repr__(self):
         return self.name
 
+    # not really needed anymore, but just so you could theoretically
+    # instantiate it as well
     def __call__(self, *args, **kwargs):
         return NAMES[self.name](*args, **kwargs)
 
@@ -84,10 +98,15 @@ class Placeholder(Addable):
         return NAMES[self.name]
 
 
+# metaclass for Tree. Needed for __add__ support, and "recursion"
 class TreeMeta(type, Addable):
     def __prepare__(name, bases, **kwargs):
         prepared = object.__prepare__(name, bases)
-        prepared[name] = Placeholder(name)  # some kind of placeholder
+        # we insert a placeholder object into the class namespace under the
+        # name of the class. That way, when executing the class body, we don't
+        # get a NameError when using "recursive" rules (such as expr -> op
+        # expr).
+        prepared[name] = Placeholder(name)
         return prepared
 
     @property
@@ -104,23 +123,24 @@ class Tree(metaclass=TreeMeta):
 
     def __init_subclass__(cls, **kwargs):
         cls.kwargs = kwargs
-        # normalization: every rules is a option of a sequence
-        if isinstance(cls.rules, Token):
-            cls.rules = Rule([cls.rules])
-        if isinstance(cls.rules, Rule):
-            cls.rules = [cls.rules]
         LEVELS.add(cls.level)
         TREES.append(cls)
         NAMES[cls.__name__] = cls
+        # normalization: if a rule outputs only a single Tree/Token, it will
+        # not be a Rule object yet, we fix that here
         cls.rules = [
             rule if isinstance(rule, Rule) else Rule([rule]) for rule in cls.rules
         ]
+        # if we have any recursive rules, we replace the placeholders with
+        # actual references to the class
         for rule in cls.rules:
             rule.parts = [
                 part.replace() if isinstance(part, Placeholder) else part
                 for part in rule.parts
             ]
 
+    # like the function parse, but enforcing that the resulting Tree is _this_
+    # kind of tree
     @classmethod
     def parse(cls, string):
         tokens = list(tokenize(string))
@@ -136,9 +156,13 @@ class Tree(metaclass=TreeMeta):
 
 
 def tokenize(string):
+    """Turn a string into a sequence of tokens."""
     while string:
+        # the sorting enforces the precedence levels for matching
         for token in sorted(TOKENS, key=lambda t: t.level):
             if match := token.regex.match(string):
+                # Tokens with ignore=True (could be e.g. Whitespace) are not
+                # emitted
                 if not token.kwargs.get("ignore", False):
                     yield token(match.string[: match.end()])
                 string = string[match.end() :]
@@ -147,11 +171,8 @@ def tokenize(string):
             raise ValueError(f"Can't tokenize remaining value: {string!r}")
 
 
-class ParseError(Exception):
-    pass
-
-
 def reduce(stack, rule, tree):
+    """The reduce step of our shift-reduce parser"""
     rule_len = len(rule)
     for item, rule_part in zip(stack[-rule_len:], rule):
         if not isinstance(item, rule_part):
@@ -161,6 +182,9 @@ def reduce(stack, rule, tree):
     return True
 
 
+# this is basically just an iterator that goes over a (sorted) sequence of
+# ints, except it can be reset to the beginning at any time. Used to enforce
+# precedence for rules.
 class LevelMaster:
     def __init__(self, levels):
         self.levels = sorted(levels, reverse=True)
@@ -181,6 +205,10 @@ class LevelMaster:
         self.index = 0
 
 
+# so here's the thing: I've done this some years ago at Uni, but I was to lazy
+# to look it up. This might not always work, might be inefficient, etc. It's
+# anyways kind of the boring part of this lib (nothing special here, except for
+# its badness)
 def parse(sequence, start=None):
     if len(sequence) == 1:
         return sequence[0]
@@ -215,6 +243,26 @@ def parse(sequence, start=None):
 old_build_class = builtins.__build_class__
 
 
+# now this _is_ fun: because there's no hook in metaclasses to customize how
+# the class body of their instances is called, we have to hook into the global
+# hook of how class definition statements are executed. We bail early if the
+# class that's being defined isn't ours, of course, but still, this seems kind
+# of messy. I'd love a __build_class__ method on the metaclass as well..
+# Anyways, the purpose of this is to transform the code from this:
+# class Foo(Tree):
+#     X
+#     Y + Z + Foo
+# into this:
+# class Foo(Tree):
+#     rules = []
+#     rules.append(X)
+#     rules.append(Y + Z + Foo)
+# We do this by adding the names "rules" and "append" to co_names, and then
+# patching the bytecode to wrap these empty, discarded statements that would
+# normally be garbage-collected into rules.append()-calls, so they can be
+# accessed later.
+# Oh, and I hope I understood co_stacksize correctly, if not, your program
+# might just crash.
 def __build_class__(func, name, *bases, **kwargs):
     if Tree in bases:
         code = func.__code__
@@ -254,6 +302,8 @@ def __build_class__(func, name, *bases, **kwargs):
         co_code.write(bytes(buffer))
         co_code = co_code.getvalue()
 
+        # the last time I did something like this, CodeType.replace didn't
+        # exist yet. Hooray!
         func.__code__ = code.replace(
             co_names=co_names,
             co_stacksize=co_stacksize,
